d9985945f307b6d880e961c36aa32846c44dea26
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-03 11:39:36 +0300

    >c++17 does not support class template argument reduction CTAD

--- vector.cpp
+++ vector.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+
 void vector_exp()
 {
-	std::vector nums{ 1,2,3,4 };
+	std::vector<int> nums{ 1,2,3,4 };
 	for (const auto& num : nums)
 	{
 		std::cout << num;
