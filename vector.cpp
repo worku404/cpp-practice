7c22e6727a0c94fefaab14a68eb9baf9e18dba80
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-03 11:56:19 +0300

    vectore practice on bush_back and insert

--- vector.cpp
+++ vector.cpp
@@ -4,8 +4,10 @@
 void vector_exp()
 {
 	std::vector<int> nums{ 1,2,3,4 };
+	nums.push_back(123);
+	nums.insert(2+nums.begin(), 321);
 	for (const auto& num : nums)
 	{
-		std::cout << num;
+		std::cout << num << '\n';
 	}
 }
