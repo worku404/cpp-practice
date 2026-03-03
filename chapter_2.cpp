37786d5b9f681b25608044c531ba729bb8ed807a
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-03 11:29:04 +0300

    main intery

--- chapter_2.cpp
+++ chapter_2.cpp
@@ -1,21 +1,72 @@
 // chapter_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-#include "input_with_test.h"
+//#include "input_with_test.h"
+//#include "input_with_exception.cpp"
+//#include "while_input.cpp"
+#include "vector.cpp"
 #include <iostream>
-
+#include <array>
 int main()
 {
-    test_code();
-    double number{};
-	std::cout << "Please enter a number: ";
+    vector_exp();
+}
+//    //test_code();
+//    std::array<double, 5u> number{};
+//    int count = 0u;
+//    while (count < number.size())
+//    {
+//        try {
+//            std::cout << "Enter a numbe: ";
+//            double result = get_number(std::cin);
+//
+//            number[count] = result;
+//            std::cout << "got you entered" << number[count] << "\n";
+//            count++;
+//            //number[count] = get_number(std::cin);
+//        }
+//        
+//        catch (std::exception & ex)
+//        {
+//            std::cout << "That is not a number\n try again\n";
+//        }
+//    }
+// 
+//    
+//}
 
-	const bool ok = get_number(std::cin, number);
-    if (ok)
-    {
-		std::cout << "You entered: " << number << '\n';
-    }
-    else {
-        std::cout << "something went wrong\n";
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //try {
+    //    double number{};
+    //    std::cout << "Please enter a number: ";
+
+    //    const double ok = get_number(std::cin);
+    //    std::cout << "You entered: " << number << '\n';
+    //}
+    //catch (const std::exception& ex)
+    //{
+    //    std::cout << "something went wrong\n";
+    //}
+  //  if (ok)
+  //  {
+  //  }
+  //  else {
+  //      std::cout << "something went wrong\n";
+  //  }
     
     //std::cout << sum(2, 4);
-}
