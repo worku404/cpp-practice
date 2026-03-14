218d357b4481f0c3f5bc7c5dd4b6a3ad93ab2c69
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-14 18:24:30 +0300

    full implimentation of utilities.cpp

---
+++ utilities.cpp
@@ -0,0 +1,15 @@
+#include <cstddef>
+#include <string>
+#include "utilities.h"
+
+Status make_ok() {
+    Status status;
+    status.success = true;
+    status.message = "";
+    status.err_position = 0;
+    return status;
+}
+
+Status make_error(const std::string& message, std::size_t err_position) {
+    return Status{false, message, err_position}
+}
