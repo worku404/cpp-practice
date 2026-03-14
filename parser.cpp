e5e569f13907c9c1d1da3da44fe141e9fe0036ef
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-14 21:45:06 +0300

    add stub code for parser

---
+++ parser.cpp
@@ -0,0 +1,11 @@
+#include "parser.h"
+#include <vector>
+
+ParseResult parse(const std::vector<Token>& tokens) {
+    (void)tokens;
+
+    ParseResult result;
+    result.root = nullptr;
+    result.status = make_error("Parser not implemented yet.", 0);
+    return result;
+}
