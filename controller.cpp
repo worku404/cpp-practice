accddcf72e21c2b32c378eb17420a847a4f2eaf6
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-14 20:36:19 +0300

    impliment stub controller logic

--- controller.cpp
+++ controller.cpp
@@ -1,9 +1,18 @@
 #include "controller.h"
+#include <istream>
 #include <ostream>
+#include <string>
 
 Status run_once(const std::string& line, std::ostream& out) {
     (void)line;
     Status status = make_error("Controller pipeline not implemented yet", 0)
         out << "error: "<<status.message<<"\n";
     return status;
+}
+
+void repl(std::istream& in, std::ostream& out) {
+    std::string line;
+    while (std::getline(in, line)) {
+        run_once(line, out);
+    }
 }
