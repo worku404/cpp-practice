f933a3c29f269712f4729dc52ffe3de9ea5859d4
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-15 05:02:12 +0300

    add helper function to remove whitespace

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,5 +1,14 @@
 #include "tokenizer.h"
+#include <cctype>
 
+namespace {
+    void skip_whitespace(const std::string& expression, std::size_t& index) {
+        while (index < expression.size() && std::isspace(static_cast<unsigned char>(expression[index])))
+        {
+            ++index;
+        }
+    }
+}
 TokenizeResult tokenize(const std::string& expression) {
     (void)expression;
 
