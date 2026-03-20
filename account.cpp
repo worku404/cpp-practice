1373d62a42bb2095bdc646d47d775881087e12fa
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-20 10:03:19 +0300

    implimenting main loop

---
+++ account.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+const int MAX_ACC = 100;
+
+// Structure for Bank Account
+struct Account {
+	int accNumber;
+	std::string name;
+	float balance;
+};
+
+// functions
+void create_account(Account acc[], int* count);
+void display_account(Account acc[], int count);
+void deposit(Account acc[], int count);
+void withdraw(Account acc[], int count);
+void check_balance(Account acc[], int count);
+
+//main funtion
+int main() {
+	Account acc[MAX_ACC];
+	int count = 0;
+	int choice;
+
+	do {
+		std::cout << "\n======= Banking System =========\n"
+			<<"1, Create Account\n"
+			<<"2, Diplay Accounts\n"
+			<<"3, Deposit\n"
+			<<"4, Withdraw\n"
+			<<"Check Balance\n"
+			<<"0, exit\n"
+			<<"> ";
+		std::cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			create_account(acc, &count);
+			break;
+		case 2:
+			display_account(acc, count);
+			break;
+		case 3:
+			deposit(acc, count);
+			break;
+		case 4:
+			withdraw(acc, count);
+			break;
+		case 5:
+			check_balance(acc, count);
+			break;
+		case 0:
+			std::cout << "Goodbay!\n";
+			break;
+		default:
+			std::cout << "Invalid choice\n";
+			break;
+		}
+	} while (choice != 0);
+
+	return 0;
+}
+
+//Create account
