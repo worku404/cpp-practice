5c281b0fad8c5f1253df22ca4cbaef9e7e622e7e
worku404 <worku.wondoson@aastustudent.edu.et>
2026-03-31 01:51:13 +0300

    nest loop + crud operation with struct

--- struct.cpp
+++ struct.cpp
@@ -24,49 +24,51 @@ struct Person
 };
 
 Person getPersonData();
-void displayPerson(const Person&);
+void display(const Person&);
 
 //Q3
 struct ComplexNum {
-	double realPart;
-	double imaginaryPart;
+	double real;
+	double imaginary;
 };
 
-ComplexNum addition(ComplexNum, ComplexNum);
-ComplexNum subtract(ComplexNum n1, ComplexNum n2);
-ComplexNum multiply(ComplexNum n1, ComplexNum n2);
-ComplexNum divide(ComplexNum n1, ComplexNum n2);
-inline void display(std::string op, ComplexNum n1, ComplexNum n2, ComplexNum r);
+ComplexNum addition(const ComplexNum& n1, const ComplexNum& n2);
+ComplexNum subtract(const ComplexNum& n1, const ComplexNum& n2);
+ComplexNum multiply(const ComplexNum& n1, const ComplexNum& n2);
+ComplexNum divide(const ComplexNum& n1, const ComplexNum& n2);
+void display(const std::string& op, const ComplexNum& n1, const ComplexNum& n2, const ComplexNum& result);
+
 int main() {
 	//Q1
-	//Phone p1, p2;
-	//p1 = { 211, 767, 8900 };
-	//p2 = getPhone();
+	Phone p1, p2;
+	p1 = { 211, 767, 8900 };
+	p2 = getPhone();
 
-	//std::cout << "phone 1\n";
-	//display(p1);
+	std::cout << "phone 1\n";
+	display(p1);
 
-	//std::cout << "phone 2\n";
-	//display(p2);
+	std::cout << "phone 2\n";
+	display(p2);
 
-	////Q2
-	//Person persons[SIZE];
+	//Q2
+	Person persons[SIZE];
 
-	//for (int i = 0; i < SIZE; i++) {
-	//	persons[i] = getPersonData();
-	//}
+	for (int i = 0; i < SIZE; i++) {
+		persons[i] = getPersonData();
+	}
 
-	//for (Person p : persons) {
-	//	displayPerson(p);
-	//}
+	for (Person p : persons) {
+		display(p);
+	}
 
+	// Q3
 	ComplexNum n1;
 	ComplexNum n2;
-	n1.realPart = 3;
-	n1.imaginaryPart = -4;
+	n1.real = 3;
+	n1.imaginary = -4;
 
-	n2.realPart = -5;
-	n2.imaginaryPart = -10;
+	n2.real = -5;
+	n2.imaginary = -10;
 	ComplexNum result = addition(n1, n2);
 	display("Addition of ", n1, n2, result);
 
@@ -110,54 +112,56 @@ Person getPersonData() {
 	return p;
 }
 
-void displayPerson(const Person& p) {
+void display(const Person& p) {
 		std::cout << std::format("Name: {}, ", p.name);
 		display(p.phone);
 }
 
 
 //Q3
-inline void display(const std::string& op, const ComplexNum& n1, const ComplexNum& n2, const ComplexNum& r) {
-	std::cout << op << n1.realPart << (n1.imaginaryPart > 0 ? " +" : " ") << n1.imaginaryPart <<
-		"i and " << n2.realPart << (n2.imaginaryPart > 0 ? " +" : " ") << n2.imaginaryPart << "i = ";
 
-	std::cout << r.realPart << (r.imaginaryPart > 0 ? " +" : " ") <<r.imaginaryPart << 'i' << std::endl;
-}
 ComplexNum addition(const ComplexNum& n1, const ComplexNum& n2) {
 	ComplexNum res;
-	res.realPart = n1.realPart + n2.realPart;
-	res.imaginaryPart = n1.imaginaryPart + n2.imaginaryPart;
+	res.real = n1.real + n2.real;
+	res.imaginary = n1.imaginary + n2.imaginary;
 	return res;
 }
 
 ComplexNum subtract(const ComplexNum& n1, const ComplexNum& n2) {
 	ComplexNum res;
-	res.realPart = n1.realPart - n2.realPart;
-	res.imaginaryPart = n1.imaginaryPart - n2.imaginaryPart;
+	res.real = n1.real - n2.real;
+	res.imaginary = n1.imaginary - n2.imaginary;
 	return res;
 }
 
 // (a+bi) * (c+di) = (ac - bd) + (ad+bc)i
 ComplexNum multiply(const ComplexNum& n1, const ComplexNum& n2) {
 	ComplexNum res;
-	res.realPart = (n1.realPart * n2.realPart) - (n1.imaginaryPart * n2.imaginaryPart);
-	res.imaginaryPart = (n1.realPart * n2.imaginaryPart) + (n1.imaginaryPart * n2.realPart);
+	res.real = (n1.real * n2.real) - (n1.imaginary * n2.imaginary);
+	res.imaginary = (n1.real * n2.imaginary) + (n1.imaginary * n2.real);
 	return res;
 }
 
 // (a+bi) / (c+di) = (ac + bd) - (bc-ad)i / c2+d2
 ComplexNum divide(const ComplexNum& n1, const ComplexNum& n2) {
 	ComplexNum res;
-	double denominator = std::pow(n2.realPart, 2) + std::pow(n2.imaginaryPart, 2);
+	double denominator = std::pow(n2.real, 2) + std::pow(n2.imaginary, 2);
 
 	if (denominator == 0) {
 		std::cout << "division by zero!";
 		return;
 	}
 
-	double ac_bd = (n1.realPart * n2.realPart) + (n1.imaginaryPart * n2.imaginaryPart);
-	double ad_bc = (n1.imaginaryPart * n2.realPart) - (n1.realPart * n2.imaginaryPart);
-	res.realPart = ac_bd / denominator;
-	res.imaginaryPart = ad_bc / denominator;
+	double ac_bd = (n1.real * n2.real) + (n1.imaginary * n2.imaginary);
+	double ad_bc = (n1.imaginary * n2.real) - (n1.real * n2.imaginary);
+	res.real = ac_bd / denominator;
+	res.imaginary = ad_bc / denominator;
 	return res;
+}
+
+void display(const std::string& op, const ComplexNum& n1, const ComplexNum& n2, const ComplexNum& r) {
+	std::cout << op << n1.real << (n1.imaginary > 0 ? " +" : " ") << n1.imaginary <<
+		"i and " << n2.real << (n2.imaginary > 0 ? " +" : " ") << n2.imaginary << "i = ";
+
+	std::cout << r.real << (r.imaginary > 0 ? " +" : " ") << r.imaginary << 'i' << std::endl;
 }
